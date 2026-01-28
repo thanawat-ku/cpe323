3a053a1cb62721a5860f32cb16c6abef64bc2401
Fook <59011305+thanawat-ku@users.noreply.github.com>
2026-01-28 14:55:08 +0700

    Update light_sleep.ino

--- Lab6/light_sleep.ino
+++ Lab6/light_sleep.ino
@@ -1,36 +1,97 @@
-/*********
-  Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete project details at https://RandomNerdTutorials.com/esp32-light-sleep-arduino/
-*********/
-
-int counter = 0;
-const int ledPin = 2;           // GPIO pin for onboard LED
-uint64_t sleepTime = 10000000;  // Sleep duration in microseconds (10 seconds)
-
+/*
+ *  ESP32
+ *  Light Sleep and wake up
+ *  by Mischianti Renzo <https://mischianti.org>
+ *
+ *  https://mischianti.org/category/tutorial/esp32-tutorial/esp32-practical-power-saving/
+ *
+ */
+ 
+#include <WiFi.h>
+#include <BluetoothSerial.h>
+#include "driver/adc.h"
+#include <esp_bt.h>
+#include <esp_wifi.h>
+#include <esp_sleep.h>
+#define STA_SSID "<YOUR-SSID>"
+#define STA_PASS "<YOUR-PASSWD>"
+ 
+BluetoothSerial SerialBT;
+ 
+int variable = 0;
+ 
 void setup() {
-    Serial.begin(115200);
-    pinMode(ledPin, OUTPUT);
-
-    // Enable wake-up by timer
-    esp_err_t result = esp_sleep_enable_timer_wakeup(sleepTime);
-
-    if (result == ESP_OK) {
-        Serial.println("Timer Wake-Up set successfully as wake-up source.");
-    } else {
-        Serial.println("Failed to set Timer Wake-Up as wake-up source.");
+    Serial2.begin(115200);
+ 
+    while(!Serial2){delay(500);}
+ 
+    SerialBT.begin("ESP32test"); //Bluetooth device name
+    SerialBT.println("START BT");
+ 
+    Serial2.println("START WIFI");
+    WiFi.begin(STA_SSID, STA_PASS);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial2.print(".");
     }
+ 
+    Serial2.println("");
+    Serial2.println("WiFi connected");
+    Serial2.println("IP address: ");
+    Serial2.println(WiFi.localIP());
+ 
+    delay(1000);
+ 
+    variable += 10;
+ 
+    Serial2.println();
+    Serial2.println("LIGHT SLEEP ENABLED FOR 5secs");
+    delay(100);
+ 
+    esp_sleep_enable_timer_wakeup(5 * 1000 * 1000);
+    esp_light_sleep_start();
+ 
+    Serial2.println();
+    Serial2.println("LIGHT SLEEP WAKE UP");
+ 
+    Serial2.print("Variable = ");
+    Serial2.println(variable);
 }
-
+ 
 void loop() {
-    Serial.printf("Counter: %d\n", counter);
-    counter++;
-
-    digitalWrite(ledPin, HIGH);  // LED on to indicate wake-up
-    delay(2000);
-    digitalWrite(ledPin, LOW);   // Turn off LED before going to sleep
-
-    Serial.println("Going into light sleep mode");
-    delay(500);
-    esp_light_sleep_start();     // Enter light sleep
-    Serial.println("Returning from light sleep");
+ 
+}
+ 
+void disableWiFi(){
+    adc_power_off();
+    WiFi.disconnect(true);  // Disconnect from the network
+    WiFi.mode(WIFI_OFF);    // Switch WiFi off
+ 
+    Serial2.println("");
+    Serial2.println("WiFi disconnected!");
+}
+void enableWiFi(){
+    adc_power_on();
+    WiFi.disconnect(false);  // Reconnect the network
+    WiFi.mode(WIFI_STA);    // Switch WiFi off
+ 
+    Serial2.println("START WIFI");
+    WiFi.begin(STA_SSID, STA_PASS);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial2.print(".");
+    }
+ 
+    Serial2.println("");
+    Serial2.println("WiFi connected");
+    Serial2.println("IP address: ");
+    Serial2.println(WiFi.localIP());
+}
+void disableBluetooth(){
+    btStop();
+    esp_bt_controller_disable();
+    delay(1000);
+    Serial2.println("BT STOP");
 }
